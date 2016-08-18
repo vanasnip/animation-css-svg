90fb65ae77b7e83a633ff971bd0777d661b2b256
Ivan Mamenemuno <IMamenemuno@ArkGroup.local>
2016-08-18 17:27:59 +0100

    Add all files cos thats whats up

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+/*
+custom.js
+*/
+alert("I am an alert box!");
+var scene = $('.scene');
+var svgs = [
+  'svg/bottom.svg',
+  'svg/fish.svg',
+  'svg/submarine.svg',
+  'svg/waves.svg'
+];
+
+function loadSvgs() {
+  $.each(svgs, function(key, url){
+    $.get(url, function(data){
+      $(data).find("svg").appendTo(scene);
+    })
+  });
+}
+
+$(document).ready(function() {
+  loadSvgs();
+});
