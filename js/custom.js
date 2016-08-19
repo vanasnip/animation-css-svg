f7a9147b92ba6947a2c77a3eea92011b56651e80
Ivan Mamenemuno <IMamenemuno@ArkGroup.local>
2016-08-19 11:27:12 +0100

    FIXED svg now loading. Background and bottom all loading, 2 errors in for loop: 1 instead of i = instead of + inside loop

--- js/custom.js
+++ js/custom.js
@@ -4,20 +4,23 @@
 
 var scene = $('.scene');
 
-var svgs = [
+
+var svgs = {
   'svg/bottom.svg': '5',
-  'svg/fish.svg': '5',
+  'svg/waves.svg': '5',
   'svg/submarine.svg': '1',
-  'svg/waves.svg': '1'
-];
+  'svg/fish.svg': '1'
+};
 
 function loadSvgs() {
   $.each(svgs, function(url, total) {
     $.get(url, function(data) {
       var svg = $(data).find("svg").first(),
       svgClass = svg.attr('class');
-      for (var i = 0; 1 < total; i++){
-        svg.attr('class', svgClass = " " + svgClass + '-' + (i + 1)).clone().appendTo(scene);
+
+
+      for (var i = 0; i < total; i++) {
+        svg.attr('class', svgClass + " " + svgClass + '-' + (i+1)).clone().appendTo(scene);
       }
     })
   });
